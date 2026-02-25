91813dcbf9b5b4086401c6a4af8ff005e4175f0e
Wallace <wallace.12.marinho@gmail.com>
2026-02-25 14:05:35 -0500

    feat: Criação da classe main para a execução do projeto

---
+++ src/br/com/wallace/biblioteca/test/SistemaDeGerenciamento.java
@@ -0,0 +1,10 @@
+package br.com.wallace.biblioteca.test;
+
+import br.com.wallace.biblioteca.model.Autor;
+import br.com.wallace.biblioteca.model.Livro;
+
+public class SistemaDeGerenciamento {
+    public static void main(String[] args) {
+
+    }
+}
