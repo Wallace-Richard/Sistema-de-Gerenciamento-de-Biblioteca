4d248a23f1f54135200ee1b15dfd955e3a786191
Wallace <wallace.12.marinho@gmail.com>
2026-02-26 19:29:56 -0500

    feat: finalização do sistema de gerenciamento da biblioteca

--- src/br/com/wallace/biblioteca/test/SistemaDeGerenciamento.java
+++ src/br/com/wallace/biblioteca/test/SistemaDeGerenciamento.java
@@ -5,6 +5,15 @@ import br.com.wallace.biblioteca.model.Livro;
 
 public class SistemaDeGerenciamento {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        Menu menuPrincipal = new Menu();
+        Biblioteca biblioteca = new Biblioteca("Biblioteca municipal");
+        menuPrincipal.setBiblioteca(biblioteca);
+        while (menuPrincipal.isDentroDoMenu())
+        {
+            Menu.exibirMenu();
+            String opcao = input.nextLine();
+            menuPrincipal.options(opcao);
+        }
     }
 }
